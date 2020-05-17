bc91d8fefb4bf948af86884533f6ff24c52f852f
Kristoffer <kristoffermeling1@gmail.com>
2020-05-17 18:45:48 +0200

    added classes file

---
+++ classes.py
@@ -0,0 +1,15 @@
+
+
+class Sensor():
+    #a class collecting sensor values
+    def __init__(self, posval, sensorheight ):
+
+        self.pos = posval
+        self.sensorheight = sensorheight
+
+
+
+
+
+
+class
