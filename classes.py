bc78f00d4bbe87236af39179233d64887e545bc1
Kristoffer <kristoffermeling1@gmail.com>
2020-05-17 21:57:46 +0200

    added stuff

--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
-
+import datetime
 
 class Sensor():
     #a class collecting sensor values
@@ -9,7 +9,19 @@ class Sensor():
 
 
 
+def les_positivt_flyttall():
+    tallet = -1.0
+    while tallet < 0.0:
+        try:
+            tallet = float(input("Skriv inn et positivt flyttall: "))
+            if tallet < 0.0:
+                print("Tallet kan ikke være negativ. Prøv på nytt!")
+        except ValueError:
+            print("Du må skrive inn et tall!")
+            tallet = -1.0
+    return tallet
 
+atring = "testavstrengslice"
 
-
-class
+test = atring[1:10:3]
+print(test)
